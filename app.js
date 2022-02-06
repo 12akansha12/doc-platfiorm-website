32e552403e342ed15540bd89a11ace075404d739
raghavraj-27 <raghvendraprasad5678@gmail.com>
2022-02-06 20:18:48 +0530

    welcome.ejs created and routed

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set("view-engine", 'ejs');
 
-app.get("/", function(req, res) {
+var workspaces = [];
+
+app.get("/sign-in", function(req, res) {
     res.sendFile(__dirname + "/signin-signup/login.html");
 });
 
 app.get("/sign-up", function(req, res) {
     res.sendFile(__dirname + "/signin-signup/signup.html");
-})
+});
+
+app.get("/", function(req, res) {
+  res.render("welcome.ejs", {workspaces_list: workspaces});
+});
+
+app.post("/", function(req, res) {
+  workspaces.push(req.body.new_workspace);
+  res.redirect("/");
+});
+
 
 app.listen(3000, function(res, req) {
   console.log("Server started on port 3000");
 });
-
