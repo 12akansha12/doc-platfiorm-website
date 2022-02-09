dfc65840e18619965abc7e6401465e2e06fadfd9
with-mahimasingh <mahimasingh503@gmail.com>
2022-02-09 20:38:26 +0530

    Added workspace with text editor

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static("public"));
 app.set("view-engine", 'ejs');
 
 var workspaces = [];
+var files = [];
 
 app.get("/sign-in", function(req, res) {
     res.sendFile(__dirname + "/signin-signup/login.html");
@@ -14,6 +15,9 @@ app.get("/sign-in", function(req, res) {
 
 app.get("/sign-up", function(req, res) {
     res.sendFile(__dirname + "/signin-signup/signup.html");
+});
+app.get("/workspace", function(req, res) {
+  res.render("workspace.ejs", {files: files});
 });
 
 app.get("/", function(req, res) {
@@ -25,7 +29,29 @@ app.post("/", function(req, res) {
   res.redirect("/");
 });
 
+app.get("/texteditor", function(req, res) {
+  res.render("texteditor");
+})
+
+app.post("/", function(req, res) {
+  
+  res.redirect("/");
+});
+
+app.post("/workspace", function(req, res) {
+  let filename = req.body.filename;
+  let filecontent = req.body.content;
+  let obj = {
+    name: filename,
+    content: filecontent
+  };
+  files.push(obj);
+  res.redirect("/workspace");
+})
+
+//------------------------------------------/
 
 app.listen(3000, function(res, req) {
   console.log("Server started on port 3000");
 });
+
